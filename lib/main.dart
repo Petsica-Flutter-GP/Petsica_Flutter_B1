52a0fb6a773ba2017ca5d24d1f6cde25946d045a
Pero Osama <peroosama03@gmail.com>
2025-03-14 19:37:42 +0200

    -jnfdjnf

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class Petsica extends StatelessWidget {
       // دعم Device Preview
       builder: DevicePreview.appBuilder,
       useInheritedMediaQuery: true,
-//jijijijijiijij
+
       theme: ThemeData(
         scaffoldBackgroundColor: kAppColor,
       ),
